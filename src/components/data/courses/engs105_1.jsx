2bce1f7ad49a6c0fda67e0d87ce540127203e106
honeybijan <bijanmazaheri@gmail.com>
2026-06-10 19:52:37 -0400

    poc notes

--- src/components/data/courses/engs105_1.jsx
+++ src/components/data/courses/engs105_1.jsx
@@ -5,7 +5,7 @@ export const engs105_1 = {
     years: ["Spring 2025", "Spring 2026"],
     description: `Helmets increase head injuries. Faster drivers arrive later. Hospital patients are less likely to have cancer when their bones are broken. But don't throw out your helmet, break your leg, or invest in transportation inefficiency just yet... Causality is the beacon of science and the foundation of policy, but causal relationships can be lost in a labyrinth of correlation. Instead of caveating correlative studies, this course establishes the principles on which scientists and engineers can answer true causal questions. At the core of this pursuit is experimentation. The class will begin by building a formal understanding of randomized control trials. We will generalize these principles to build causal inference methods for observational (i.e., non-experimental) data. As we dig deeper into these methods, we will discover that causal structure plays a key role in their correctness. The final portion of this class will focus on the task of learning these causal structures using observational data and/or efficient experimental design.`,
     downloads: {
-      notes: null,
+      notes: "/public/Principles_of_Causality_Textbook.pdf",
       homeworks: null,
     },
   };
